09e7a7d739c6b7ecb8f1f653fa201fcbf0e21955
kenta yamada <pandorina463@gmail.com>
2020-05-05 15:22:24 +0900

    fix splitter bug

--- src/splitter.py
+++ src/splitter.py
@@ -1,71 +1,36 @@
-import xml.etree.ElementTree as ET
 import pandas as pd
-import numpy as np
+import os
+
+from splitter import Splitter
 
 
-class Splitter():
+def merge_all():
+    i = 0
+    for fname in os.listdir('output'):
+        if '.csv' in fname:
+            if i == 0:
+                df = pd.read_csv('output/' + fname)
+            else:
+                df = pd.merge(df, pd.read_csv('output/' + fname),
+                            how='outer', on='datetime')
+            i+=1
+    df.to_csv('health_care.csv')
 
-    def __init__(self):
-        self.root = ET.parse('input/export.xml').getroot()
 
-    def get_step_count(self):
-        df = self.xml_to_csv("HKQuantityTypeIdentifierStepCount")
-        df = df.groupby('datetime').sum()
-        df.to_csv("output/step_count.csv")
-        print("step count <- done.")
+if __name__ == "__main__":
 
-    def get_heart_rate(self):
-        df = self.xml_to_csv("HKQuantityTypeIdentifierHeartRate")
-        df = df[df['HKQuantityTypeIdentifierHeartRate'] > 0]
-        df = df.groupby('datetime').agg({np.min, np.max})
-        df.columns = [
-            'HKQuantityTypeIdentifierHeartRateMin',
-            'HKQuantityTypeIdentifierHeartRateMax']
-        df.to_csv("output/heart_rate.csv")
-        print("heart rate <- done.")
+    print("Convert apple health care xml to csv.")
 
-    def get_body_mass(self):
-        df = self.xml_to_csv("HKQuantityTypeIdentifierBodyMass")
-        df = df.groupby('datetime').sum()
-        df.to_csv("output/body_mass.csv")
-        print("body mass <- done.")
+    s = Splitter()
 
-    def get_burned_energy(self):
-        df = self.xml_to_csv("HKQuantityTypeIdentifierActiveEnergyBurned")
-        df = df.groupby('datetime').sum()
-        df.to_csv("output/burned_energy.csv")
-        print("burned energy <- done.")
+    s.get_body_mass()
+    s.get_burned_energy()
+    s.get_heart_rate()
+    s.get_stand_time()
+    s.get_step_count()
+    s.get_walking_distance()
 
-    def get_walking_distance(self):
-        df = self.xml_to_csv("HKQuantityTypeIdentifierDistanceWalkingRunning")
-        df = df.groupby('datetime').sum()
-        df.to_csv("output/warking_distance.csv")
-        print("distance <- done.")
+    print("Merge all csv.")
+    merge_all()
 
-    def get_stand_time(self):
-        df = self.xml_to_csv("HKQuantityTypeIdentifierAppleStandTime")
-        df = df.groupby('datetime').sum()
-        df.to_csv("output/stand_time.csv")
-        print("standtime <- done.")
-
-    def xml_to_csv(self, type):
-        datetime, counts = [], []
-
-        for child in self.root:
-            data = child.attrib
-
-            try:
-                if data['type'] == type:
-                    datetime.append(data['startDate'])
-                    counts.append(data['value'])
-            except Exception:
-                pass
-
-        df = pd.DataFrame({type: counts})
-
-        df.index = pd.to_datetime(datetime, utc=True)
-        df['datetime'] = df.index.tz_convert('Asia/Tokyo')
-        df['datetime'] = df.datetime.dt.date
-        df[type] = df[type].astype(float)
-
-        return df
+    print("Done.")
